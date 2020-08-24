111a9e3cf0bb3aba7f57cc79a6f2e9dc5e96190d
malgorzatasobocinska <sobocinska.malgo@gmail.com>
2020-08-24 16:41:51 +0200

    Initial commit

---
+++ src/main/java/pl/sobocinska/BusyBooks/BusyBooksApplication.java
@@ -0,0 +1,13 @@
+package pl.sobocinska.BusyBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusyBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sobocinska/BusyBooks/BusyBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sobocinska.BusyBooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusyBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
